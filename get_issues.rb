855a12070c4aaac1a942aa79b5ca713fc4cf3961
Yusuke Yasuo <yasuo.yusuke@redish.jp>
2019-01-07 00:32:27 +0900

    改行をスペースに変換する

--- get_issues.rb
+++ get_issues.rb
@@ -8,7 +8,7 @@ res_csv = ''
 res = Net::HTTP.get(URI.parse("https://api.github.com/repos/rails/rails/issues?page=#{page}&per_page=#{per_page}"))
 issues = JSON.parse(res)
 issues.each do |i|
-  res_csv += "#{i['title'].slice(0, 30)},#{i['body'].slice(0, 50)},#{i['comments_url']}\n"
+  res_csv += "#{i['title'].slice(0, 30)},#{i['body'].slice(0, 50).gsub(/(\r\n?|\n)/," ")},#{i['comments_url']}\n"
 end
 
 print res_csv
